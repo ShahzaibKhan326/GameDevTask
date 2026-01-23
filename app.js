dbfaa71754422aeecb8b2fa21220e0573ab9e3e4
ShahzaibKhan326 <kshahzaib07@gmail.com>
2026-01-23 09:38:23 -0800

    update

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const hero = document.querySelector("#Hulk");
 console.log(hero)
 
-const stance = "Assets/Hero/hulk-win.gif"
+const stance = "Assets/Hero/hulk-stance.gif"
 const walkFarward = "Assets/Hero/hulk-walk.gif"
 const walkBackward = "Assets/Hero/hulk-walkBack.gif"
 const wins = "Assets/Hero/hulk-win.gif"
+const onJump = "Assets/Hero/hulk-intocrouch.gif"
+
+
 
 window.addEventListener("keydown",(e)=>
 {
     if(e.key === "ArrowRight")
     {
-        alert("keyRight");
+        hero.src = walkFarward;
     }
     if(e.key === "ArrowLeft")
     {
-        alert("keyDown");
+        hero.src = walkBackward;
     }
      if(e.key === " ")
     {
-        alert("Space");
+     hero.src = onJump
     }
 
 })
